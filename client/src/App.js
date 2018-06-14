6b5ff1487c970aaa5dd467a4b9f725831384aee5
Dereje1 <getahun.d@gmail.com>
2018-06-14 12:24:31 -0400

    verified server communication

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,23 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props){
+    super(props)
+    this.state={
+      fromServer:''
+    }
+  }
+  componentDidMount(){
+    fetch('/api')
+    .then((response)=>{
+      return response.json()
+    })
+    .then((serverMessage)=>{
+      this.setState({
+        fromServer:serverMessage
+      })
+    })
+  }
   render() {
     return (
       <div className="App">
@@ -11,6 +28,8 @@ class App extends Component {
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
+          {this.state.fromServer}
+          <br/>
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
       </div>
--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express()
 const port = process.env.port || 5000
 
 app.get("/api",(req,res)=>{
-    res.json("Hello from server Bears Team 05!!")
+    res.json("Hello Bears Team 05!!")
 })
 
 const listener = app.listen(port,()=>{
