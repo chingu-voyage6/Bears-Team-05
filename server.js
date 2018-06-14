cdd9139c6a72e77a730871a8a3396444188311ca
Dereje1 <getahun.d@gmail.com>
2018-06-14 12:12:46 -0400

    CRA setup , express setup, proxy setup

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express')
+
+const app = express()
+const port = process.env.port || 5000
+
+app.get("/api",(req,res)=>{
+    res.json("Hello from server Bears Team 05!!")
+})
+
+const listener = app.listen(port,()=>{
+    console.log(`Server listening on port ${listener.address().port}`)
+})
