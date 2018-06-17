7dac95c59403269c860027de9c24fb77e524e8a2
Jay Schwane <jrschwane@uwalumni.com>
2018-06-17 17:53:23 -0500

    Updates reducers to satisfy ESLint

--- client/src/Reducers/testReducer.js
+++ client/src/Reducers/testReducer.js
@@ -1,13 +1,13 @@
-//test reducer sample for making individual reducers
+// test reducer sample for making individual reducers
+export default (state = '', action) => {
+  switch (action.type) {
+    case 'BEGIN':
+      return 'Testing has Begun';
+    case 'END':
+      return 'Testing has Ended';
+    default:
+      break;
+  }
 
-export const testReducer = (state='',action)=>{
-    switch (action.type) {
-        case 'BEGIN':
-            return 'Testing has Begun'
-        case 'END':
-            return 'Testing has Ended'
-        default:
-            break;
-    }
-    return state
-}
+  return state;
+};
