3cbb149f29116babef0f62364936fa6e6fe28f8c
Dereje1 <getahun.d@gmail.com>
2018-06-26 21:51:47 -0400

    config passport, added auth route and model,verified server side oauth,

--- server/index.js
+++ server/index.js
@@ -7,21 +7,36 @@ const cors = require('cors');
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
 const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
 
 module.exports = { io };
 const socketManager = require('./services/socketManager');
-const router = require('./router');
+const router = require('./routes/router');
+const authRoutes = require('./routes/authRoutes');
 
+app.use(bodyParser.json({ limit: '5mb' }));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
+// set up session cookies
+app.use(cookieSession({
+  maxAge: 21 * 24 * 60 * 60 * 1000,
+  keys: [process.env.COOKIE_KEY],
+}));
+
+// initialize passport
+require('./services/passportConfig');
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 mongoose.connect(process.env.MONGOLAB_URI, () => {
   console.log(`Connected to ${process.env.MONGOLAB_URI}`);
 });
 mongoose.set('debug', true);
 
-app.use(bodyParser.json({ limit: '5mb' }));
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cors());
-// app.use(express.static('public'));
+app.use(authRoutes);
+
 router(app);
 io.on('connection', socketManager);
 
---
+++ server/services/passportConfig.js
@@ -0,0 +1,39 @@
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
+const User = require('../../models/user');
+
+// get user info in session
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+  User.findById(id).then((user) => {
+    done(null, user);
+  });
+});
+
+// Google Strategy
+passport.use(new GoogleStrategy(
+  {
+    clientID: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    callbackURL: '/auth/google/redirect',
+  },
+  async (accessToken, refreshToken, profile, done) => {
+    const currentUser = await User.findOne({ 'google.id': profile.id });
+    if (currentUser) {
+      done(null, currentUser);
+    } else {
+      const newUser = await new User({
+        google: {
+          id: profile.id,
+          displayName: profile.displayName,
+          email: profile.emails[0].value,
+          username: null,
+        },
+      }).save();
+      done(null, newUser);
+    }
+  },
+));
