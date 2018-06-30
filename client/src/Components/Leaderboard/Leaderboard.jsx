8a3b7e2f764e4734a465ef47b2e9a977e10fb0a2
eolculnamo2 <rbertram8@gmail.com>
2018-06-29 23:10:59 -0500

    leaderboard

--- client/src/Components/Leaderboard/Leaderboard.jsx
+++ client/src/Components/Leaderboard/Leaderboard.jsx
@@ -1,9 +1,54 @@
 import React from 'react';
 import './Leaderboard.css';
 
+let dummyData = [
+  {
+    username: "Quincy",
+    bestScore: Math.round(Math.random()*1000)
+  },
+  {
+    username: "Dave",
+    bestScore: Math.round(Math.random()*1000)
+  },
+  {
+    username: "Joseph",
+    bestScore: Math.round(Math.random()*1000)
+  },
+  {
+    username: "Donald",
+    bestScore: Math.round(Math.random()*1000)
+  },
+  {
+    username: "Danica",
+    bestScore: Math.round(Math.random()*1000)
+  }
+]
+
+dummyData.sort((a,b) => {
+  return b.bestScore-a.bestScore;
+})
+
 const Leaderboard = () => (
   <div className="leaderboard">
-    Leaderboard goes here
+    <div className="board">
+      <div className="board-header">
+        High Scores
+      </div>
+        {dummyData.map((x,i) => {
+          if (i < 10) {
+            return (
+              <div className="leaderboard-player">
+                <p>
+                  {x.username}
+                </p>
+                <p>
+                  {x.bestScore}
+                </p>
+              </div>
+            )
+          }
+        })}
+    </div>
   </div>
 );
 
