3f6e3f2f8c9229aebfe3ec7e63081ba82ad4b027
Terrance Xin <xin.terrance@gmail.com>
2018-07-08 20:05:57 -0400

    Add constants file

---
+++ constants/index.js
@@ -0,0 +1,9 @@
+module.exports = {
+  SOCKET_EVENTS: [
+    'USER_CONNECTED',
+    'USER_DISCONNECTED',
+  ],
+  GET_USER_STATUS: 'GET_USER_STATUS',
+  serverUrl: () => process.env.SERVER_URL || 'http://localhost:5000',
+  clientUrl: () => process.env.CLIENT_URL || 'http://localhost:3000',
+};
