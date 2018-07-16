b2421e156a3b60b59b94e1b347751c67ffea4841
Jay Schwane <jrschwane@uwalumni.com>
2018-07-15 23:26:13 -0500

    Rearrange server; removes unused code/modules; refactors route calls

--- server/index.js
+++ server/index.js
@@ -1,19 +1,30 @@
-const express = require('express');
+/* ================================= SETUP ================================= */
 
-const app = express();
 require('dotenv').config();
+
+const ENV = process.env.NODE_ENV || 'development';
+const PORT = process.env.PORT || 5000;
+
+const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const http = require('http').Server(app);
-const io = require('socket.io')(http);
 const mongoose = require('mongoose');
 const cookieSession = require('cookie-session');
 const passport = require('passport');
+const dbConfig = require('./db')[ENV];
+
+const app = express();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
 
 module.exports = { io };
 const socketManager = require('./services/socketManager');
-const router = require('./routes/router');
-const authRoutes = require('./routes/authRoutes');
+
+// initialize passport
+require('./services/passportConfig');
+
+
+/* ============================== MIDDLEWARE =============================== */
 
 app.use(bodyParser.json({ limit: '5mb' }));
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -24,24 +35,27 @@ app.use(cookieSession({
   keys: [process.env.COOKIE_KEY],
 }));
 
-// initialize passport
-require('./services/passportConfig');
-
 app.use(passport.initialize());
 app.use(passport.session());
 
-mongoose.connect(process.env.MONGOLAB_URI, () => {
-  console.log(`Connected to ${process.env.MONGOLAB_URI}`);
-});
+
+/* ================================ ROUTERS ================================ */
+
+app.use('/auth', require('./routes/authRoutes'));
+app.use('/api', require('./routes/apiRoutes'));
+
+
+/* ======================= CONNECT TO DB AND STARTUP ======================= */
+
+mongoose.connect(dbConfig.url)
+  .then(() => {
+    console.log(`Connected to ${dbConfig.url}`);
+    http.listen(PORT, () => console.log('Server listening on port:', PORT));
+    io.on('connection', socketManager);
+  })
+  .catch((err) => {
+    console.log(err.message);
+    process.exit(1);
+  });
+
 mongoose.set('debug', true);
-
-app.use(authRoutes);
-
-router(app);
-io.on('connection', socketManager);
-
-const PORT = process.env.PORT || 5000;
-
-http.listen(PORT, () => {
-  console.log('Server listening on port:', PORT);
-});
