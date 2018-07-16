36204bca3c6733233c244c748c6c627d5aac4cb9
Jay Schwane <jrschwane@uwalumni.com>
2018-07-16 14:12:48 -0500

    Adds getPlayersRecentMatches route handler

--- server/controllers/matchCtrl.js
+++ server/controllers/matchCtrl.js
@@ -1,10 +1,58 @@
 /* ================================= SETUP ================================= */
 
 const Match = require('../models/match');
-
+const { ObjectId } = require('mongoose').Types;
 
 /* ============================== CONTROLLERS ============================== */
 
+/**
+ * Get all player's recent match results
+ * Example: GET >> /api/my_results/5b4cd14d343984146b6dd676?limit=10
+ * Secured: not yet -- authentication should be required though
+ * Expects:
+ *    1) req.params.playerId   {String}   Player's _id
+ *    2) req.query.limit       {Number}   Qty of matches to return. Default: 10
+ * Returns: JSON object with arrays for most-recent single &
+ * multi-player matches
+*/
+const getPlayersRecentMatches = (req, res) => {
+  const { playerId } = req.params;
+  const limit = parseInt(req.query.limit, 10) || 10;
+
+  Match.aggregate([
+    { $match: { 'players._id': ObjectId(playerId) } },
+    { $sort: { createdAt: -1 } },
+    {
+      $group: {
+        _id: { multiPlayer: '$multiPlayer' },
+        matches: { $push: '$$ROOT' },
+      },
+    },
+    {
+      $project: {
+        _id: 0,
+        matches: { $slice: ['$matches', limit] },
+      },
+    },
+    {
+      $project: {
+        'matches._id': 0,
+        'matches.multiPlayer': 0,
+        'matches.updatedAt': 0,
+        'matches.__v': 0,
+      },
+    },
+  ])
+    .then(([multiPlayer, singlePlayer]) => res.json({ singlePlayer, multiPlayer }));
+};
+
+/**
+ * Get leaderboard data
+ * Example: GET >> /api/leaderboard
+ * Secured: no
+ * Returns: JSON object with arrays for top single player scores & most
+ * multi-player wins
+*/
 const getLeaderboard = (req, res) => {
   const getSinglePlayerLeaders = Match.aggregate([
     { $match: { multiPlayer: false } },
@@ -53,4 +101,5 @@ const getLeaderboard = (req, res) => {
 
 module.exports = {
   getLeaderboard,
+  getPlayersRecentMatches,
 };
