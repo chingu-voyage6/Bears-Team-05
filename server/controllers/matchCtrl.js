cad23b5f70bb01618742348d6eabc0ca5a78ae54
Jay Schwane <jrschwane@uwalumni.com>
2018-07-17 09:09:41 -0500

    Moves all DB logic into Match schema static methods

--- server/controllers/matchCtrl.js
+++ server/controllers/matchCtrl.js
@@ -1,7 +1,7 @@
 /* ================================= SETUP ================================= */
 
 const Match = require('../models/match');
-const { ObjectId } = require('mongoose').Types;
+
 
 /* ============================== CONTROLLERS ============================== */
 
@@ -9,46 +9,18 @@ const { ObjectId } = require('mongoose').Types;
  * Get leaderboard data
  * Example: GET >> /api/leaderboard
  * Secured: no
+ * Expects:
+ *    1) req.query.limit   {Number}   Optional num of matches. Default: 10
  * Returns: JSON object with arrays for top single player scores & most
  * multi-player wins
 */
 const getLeaderboard = (req, res, next) => {
-  const getSinglePlayerLeaders = Match.aggregate([
-    { $match: { multiPlayer: false } },
-    { $unwind: '$players' },
-    { $sort: { 'players.score': -1 } },
-    { $limit: 10 },
-    {
-      $project: {
-        _id: 0,
-        name: '$players.name',
-        score: '$players.score',
-      },
-    },
-  ]);
+  const limit = parseInt(req.query.limit, 10) || 10;
 
-  const getMultiPlayerLeaders = Match.aggregate([
-    { $match: { multiPlayer: true } },
-    { $unwind: '$players' },
-    { $match: { 'players.winner': true } },
-    {
-      $group: {
-        _id: { name: '$players.name' },
-        wins: { $sum: 1 },
-      },
-    },
-    { $sort: { wins: -1 } },
-    { $limit: 10 },
-    {
-      $project: {
-        _id: 0,
-        name: '$_id.name',
-        wins: 1,
-      },
-    },
-  ]);
-
-  Promise.all([getSinglePlayerLeaders, getMultiPlayerLeaders])
+  Promise.all([
+    Match.getSinglePlayerLeaders(limit),
+    Match.getMultiPlayerLeaders(limit),
+  ])
     .then(([spScores, mpScores]) => res.json({ spScores, mpScores }))
     .catch(err => next(err));
 };
@@ -57,34 +29,18 @@ const getLeaderboard = (req, res, next) => {
  * Get all players' top single-player scores
  * Example: GET >> /api/best_scores
  * Secured: no
+ * Expects: nothing
  * Returns: JSON array of all players' best single-player scores
 */
 const getPlayersBestScores = (req, res, next) => {
-  Match.aggregate([
-    { $match: { multiPlayer: false } },
-    { $unwind: '$players' },
-    {
-      $group: {
-        _id: { name: '$players.name' },
-        bestScore: { $max: '$players.score' },
-      },
-    },
-    {
-      $project: {
-        _id: 0,
-        name: '$_id.name',
-        bestScore: 1,
-      },
-    },
-    { $sort: { name: 1 } },
-  ])
+  Match.getAllBestSPScores()
     .then(bestScores => res.json(bestScores))
     .catch(err => next(err));
 };
 
 /**
  * Get all player's recent match results
- * Example: GET >> /api/my_results?limit=5
+ * Example: GET >> /api/my_results?limit=20
  * Secured: yes
  * Expects:
  *    1) req.user._id      {String}   Player's user _id
@@ -93,34 +49,17 @@ const getPlayersBestScores = (req, res, next) => {
  * multi-player matches
 */
 const getPlayersRecentMatches = (req, res, next) => {
+  if (!req.user || !req.user._id) {
+    return res.status(400).json({ message: 'Missing required parameters.' });
+  }
   const playerId = req.user._id;
-  const limit = parseInt(req.query.limit, 10) || 10;
+  const limit = parseInt(req.query.limit, 10) || 2;
 
-  Match.aggregate([
-    { $match: { 'players._id': ObjectId(playerId) } },
-    { $sort: { createdAt: -1 } },
-    {
-      $group: {
-        _id: { multiPlayer: '$multiPlayer' },
-        matches: { $push: '$$ROOT' },
-      },
-    },
-    {
-      $project: {
-        _id: 0,
-        matches: { $slice: ['$matches', limit] },
-      },
-    },
-    {
-      $project: {
-        // 'matches._id': 0,
-        'matches.multiPlayer': 0,
-        'matches.updatedAt': 0,
-        'matches.__v': 0,
-      },
-    },
-  ])
-    .then(([mpMatches, spMatches]) => res.json({ spMatches, mpMatches }))
+  return Match.getOwnRecentMatches(playerId, limit)
+    .then(([mpMatches, spMatches]) => res.json({
+      spMatches: spMatches.matches,
+      mpMatches: mpMatches.matches,
+    }))
     .catch(err => next(err));
 };
 
--- server/models/match.js
+++ server/models/match.js
@@ -1,4 +1,11 @@
+/* ================================= SETUP ================================= */
+
 const mongoose = require('mongoose');
+
+const { ObjectId } = mongoose.Types;
+
+
+/* ================================ SCHEMA ================================= */
 
 const matchSchema = new mongoose.Schema(
   {
@@ -16,5 +23,101 @@ const matchSchema = new mongoose.Schema(
     timestamps: true,
   },
 );
+
+
+/* ============================ SCHEMA METHODS ============================= */
+
+matchSchema.statics.getAllBestSPScores = function getAllBestSPScores() {
+  return this.aggregate([
+    { $match: { multiPlayer: false } },
+    { $unwind: '$players' },
+    {
+      $group: {
+        _id: { playerId: '$players._id', name: '$players.name' },
+        bestScore: { $max: '$players.score' },
+      },
+    },
+    {
+      $project: {
+        _id: '$_id.playerId',
+        name: '$_id.name',
+        bestScore: 1,
+      },
+    },
+    { $sort: { name: 1 } },
+  ]);
+};
+
+matchSchema.statics.getSinglePlayerLeaders = function getSPLeaders(limit) {
+  return this.aggregate([
+    { $match: { multiPlayer: false } },
+    { $unwind: '$players' },
+    { $sort: { 'players.score': -1 } },
+    { $limit: limit },
+    {
+      $project: {
+        _id: 0,
+        name: '$players.name',
+        score: '$players.score',
+      },
+    },
+  ]);
+};
+
+matchSchema.statics.getMultiPlayerLeaders = function getMPLeaders(limit) {
+  return this.aggregate([
+    { $match: { multiPlayer: true } },
+    { $unwind: '$players' },
+    { $match: { 'players.winner': true } },
+    {
+      $group: {
+        _id: { name: '$players.name' },
+        wins: { $sum: 1 },
+      },
+    },
+    { $sort: { wins: -1 } },
+    { $limit: limit },
+    {
+      $project: {
+        _id: 0,
+        name: '$_id.name',
+        wins: 1,
+      },
+    },
+  ]);
+};
+
+matchSchema.statics.getOwnRecentMatches = function getOwnRecentMatches(
+  playerId,
+  limit,
+) {
+  return this.aggregate([
+    { $match: { 'players._id': ObjectId(playerId) } },
+    { $sort: { createdAt: -1 } },
+    {
+      $group: {
+        _id: { multiPlayer: '$multiPlayer' },
+        matches: { $push: '$$ROOT' },
+      },
+    },
+    {
+      $project: {
+        _id: 0,
+        matches: { $slice: ['$matches', limit] },
+      },
+    },
+    {
+      $project: {
+        'matches._id': 0,
+        'matches.multiPlayer': 0,
+        'matches.updatedAt': 0,
+        'matches.__v': 0,
+      },
+    },
+  ]);
+};
+
+
+/* ================================ EXPORTS ================================ */
 
 module.exports = mongoose.model('Match', matchSchema);
