bc26dc9ff839ad2049103bffb0c11d480e185d7e
Jay Schwane <jrschwane@uwalumni.com>
2018-07-25 01:12:34 -0500

    Client: moves any/all state logic in to of Reducers/gameReducer.js

--- client/src/Reducers/gameReducer.js
+++ client/src/Reducers/gameReducer.js
@@ -1,43 +1,110 @@
 import {
-  INITIALIZE_GAME, LEVEL_UP, PAUSE,
-  GET_NEXT_SHAPE, SCREEN_UPDATE, RAISE_FLOOR, COLLISION,
-} from '../constants/index';
+  COLLISION,
+  GET_NEXT_SHAPE,
+  LEVEL_UP,
+  PAUSE,
+  RAISE_FLOOR,
+  RESET_GAME,
+  SCREEN_UPDATE,
+} from '../constants';
 
-const gameReducer = (state = {}, action) => {
+import { setBoundry, getNewOccupiedCells, getNewBoundary } from '../utils';
+
+const initialState = {
+  timerInterval: 700,
+  paused: false,
+  nextShape: '',
+  canvas: {
+    canvasMajor: {
+      width: 300,
+      height: 600,
+    },
+    canvasMinor: {
+      width: 210,
+      height: 150,
+    },
+  },
+  points: {
+    totalLinesCleared: 0,
+    level: 0,
+    levelUp: 5,
+  },
+  rubble: {
+    occupiedCells: [],
+    winRows: null,
+    boundaryCells: [],
+  },
+  activeShape: {
+    name: 'shapeZ',
+    unitBlockSize: 30,
+    xPosition: 0,
+    yPosition: 0,
+    unitVertices: [],
+    absoluteVertices: [],
+    boundingBox: [],
+    rotationStage: 0,
+    cells: [],
+  },
+};
+
+const gameReducer = (state = initialState, action) => {
   switch (action.type) {
-    case INITIALIZE_GAME:
-      return action.payload;
+    case RESET_GAME:
+      return {
+        ...initialState,
+        rubble: {
+          ...initialState.rubble,
+          boundaryCells: setBoundry(state.activeShape, initialState.canvas.canvasMajor),
+        },
+      };
+
     case LEVEL_UP:
-      return Object.assign({}, state, {
+      return {
+        ...state,
         timerInterval: state.timerInterval - action.payload,
-      });
+      };
+
     case PAUSE:
-      return Object.assign({}, state, {
+      return {
+        ...state,
         paused: action.payload,
-      });
+      };
+
     case GET_NEXT_SHAPE:
-      return Object.assign({}, state, {
+      return {
+        ...state,
         nextShape: action.payload,
-      });
+      };
+
     case SCREEN_UPDATE:
-      return Object.assign({}, state, {
+      return {
+        ...state,
         activeShape: action.payload.activeShape,
         rubble: action.payload.rubble,
         paused: action.payload.paused,
-      });
+      };
+
     case RAISE_FLOOR:
-      return Object.assign({}, state, {
-        rubble: action.payload,
-      });
+      return {
+        ...state,
+        rubble: {
+          ...state.rubble,
+          occupiedCells: getNewOccupiedCells(action.payload),
+          boundaryCells: getNewBoundary(action.payload, state),
+          winRows: null,
+        },
+      };
+
     case COLLISION:
-      return Object.assign({}, state, {
+      return {
+        ...state,
         rubble: action.payload.rubble,
         points: action.payload.points,
-      });
+      };
+
     default:
-      break;
+      return state;
   }
-  return state;
 };
 
 export default gameReducer;
