aded92f31ad300183577e4466dccde921845c309
Jay Schwane <jrschwane@uwalumni.com>
2018-07-25 01:14:02 -0500

    Client store: temporarily disables redux-logger

--- client/src/store/store.js
+++ client/src/store/store.js
@@ -1,11 +1,11 @@
 import { applyMiddleware, createStore } from 'redux';
 import thunk from 'redux-thunk';
-import logger from 'redux-logger';
+// import logger from 'redux-logger';
 import rootReducer from '../Reducers';
 
 const store = createStore(
   rootReducer,
-  applyMiddleware(thunk, logger),
+  applyMiddleware(thunk),
 );
 
 export default store;
