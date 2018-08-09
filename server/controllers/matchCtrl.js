ef5d41fdf48c9f973308ef215102159d4a5b8573
Dereje1 <getahun.d@gmail.com>
2018-08-09 16:17:32 -0500

    remove console.log and logger

--- server/controllers/matchCtrl.js
+++ server/controllers/matchCtrl.js
@@ -48,7 +48,6 @@ const getPlayersBestScores = (req, res, next) => {
  *    multiplayer games played, wins/losses
 */
 const getUsersOwnStats = (req, res, next) => {
-  console.log(req.user);
   if (!req.user || !req.user._id) {
     return res.status(400).json({ message: 'Authentication required.' });
   }
